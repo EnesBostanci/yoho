5d7307b14a941b056ac6f5ddd8a6c3aa2c7d09f1
EnesBostanci <76067869+EnesBostanci@users.noreply.github.com>
2025-10-30 11:40:24 +0300

    Refactor WhatIs component layout for improved responsiveness and consistency. Adjusted width properties for text and image elements based on the reverse prop.

--- src/components/sections/WhatIs.tsx
+++ src/components/sections/WhatIs.tsx
@@ -7,11 +7,11 @@ export default function WhatIs({header,headerSize,button,reverse}){
     <section className="relative">
         <Image  alt="layer" src="background.svg" fill className="z-0  object-cover"/>
             <Container className={`flex flex-col gap-4 lg:justify-center lg:flex-row ${reverse ?"lg:flex-row-reverse lg:mb-150 lg:mt-100":"" }`}>
-                <div className={`text-center ${reverse ?"lg:ml-auto":"lg:mr-auto"} `}>
+                <div className={`text-center ${reverse ?"lg:ml-auto lg:w-1/2":"lg:mr-auto lg:w-1/2"} `}>
                 <h2 className={`font-bold text-4xl ${headerSize} text-text uppercase leading-tight z-10 `}>{header}</h2>
                 {button && <Button title="YOU & YOHO" bgColor="bg-button" textColor="white"/>}
                 </div>
-                <Image width={900} height={700} alt="layer" src="layer_1.svg" className={reverse ?"lg:absolute left-0 mb-10 lg:-top-40 z-10":"lg:absolute right-0 lg:-top-40 z-10" }/>
+                <Image width={900} height={700} alt="layer" src="layer_1.svg" className={reverse ?"lg:absolute left-0 mb-10 lg:-top-40 z-10 lg:w-1/2":"lg:absolute right-0 lg:-top-40 z-10 lg:w-1/2" }/>
             </Container>
     </section>
         );
